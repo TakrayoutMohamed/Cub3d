afc31dc9690f0db5a089e6ab7d794b2419412e77
TakrayoutMohamed <takrayoutm@gmail.com>
2023-12-08 15:22:47 +0100

    release

--- release.c
+++ release.c
@@ -6,7 +6,7 @@
 /*   By: mohtakra <mohtakra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 13:30:46 by abenheni          #+#    #+#             */
-/*   Updated: 2023/12/08 15:20:04 by mohtakra         ###   ########.fr       */
+/*   Updated: 2023/12/08 15:22:15 by mohtakra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,12 @@ int	release(int key, t_data *info)
 {
 	t_player	*player;
 
-	(void) key;
 	player = &info->_player;
-	player->walk_direction = 0;
-	player->turn_direction = 0;
-	return (0);
+	if (key == KEY_A || key == KEY_D || key == KEY_W || key == KEY_S)
+	{
+		player->walk_direction = 0;
+	}
+	if (key == RIGHT_ARROW || key == LEFT_ARROW)
+		player->turn_direction = 0;
+	return (1);
 }
